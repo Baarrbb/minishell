886d3d1653df5ff482c8acfe9d37079ed67e7627
Barbara Suc <bsuc@student.42paris.fr>
2024-01-09 20:48:09 +0100

    probleme quote colle

--- testter.c
+++ testter.c
@@ -66,18 +66,70 @@ static int	get_nb_args(char **quote)
 	count = 0;
 	while (quote[++i])
 	{
+		printf("quote[i] .%s.\n", quote[i]);
 		last_pos = ft_strlen(quote[i]) - 1;
 		if ((quote[i][0] == '\"' && quote[i][last_pos] == '\"')
 			|| (quote[i][0] == '\'' && quote[i][last_pos] == '\''))
 			count++;
 		else if (quote[i][0] == '\"')
 		{
-			while (quote[i][last_pos] != '\"')
+			while (quote[i][last_pos] != '\"' && quote[i] != 0)
+			{
+				printf("qui] .%s.\n", quote[i]);
+				i++;
+				printf("qui++] .%s.\n", quote[i]);
+				if (quote[i] != 0)
+				{
+					printf("je ne rentre pas\n");
+					last_pos = ft_strlen(quote[i]) - 1;
+				}
+			}
+			printf("seg %d??\n", i);
+			count++;
+		}
+		else if (quote[i][0] == '\'')
+		{
+			while (quote[i][last_pos] != '\'' && quote[i])
 			{
 				i++;
 				last_pos = ft_strlen(quote[i]) - 1;
 			}
 			count++;
+		}
+		else
+			count++;
+	}
+	printf("count %d\n", count);
+	return (count);
+}
+
+static char **get_args_w_quote(char **quote, int nb_args, char *line_quote)
+{
+	int		i;
+	int		j;
+	int		last_pos;
+	char	**good_quote;
+
+	i = -1;
+	j = 0;
+	good_quote = ft_calloc(nb_args + 1, sizeof(char *));
+	if (!good_quote)
+		return (0);
+	while (quote[++i])
+	{
+		last_pos = ft_strlen(quote[i]) - 1;
+		if ((quote[i][0] == '\"' && quote[i][last_pos] == '\"')
+			|| (quote[i][0] == '\'' && quote[i][last_pos] == '\''))
+			good_quote[j] = ft_strdup(quote[i]);
+		else if (quote[i][0] == '\"')
+		{
+			good_quote[j] = ft_strdup(quote[i]);
+			while (quote[i][last_pos] != '\"')
+			{
+				i++;
+				last_pos = ft_strlen(quote[i]) - 1;
+				good_quote[j] = strjoin(good_quote[j], quote[i]);
+			}
 		}
 		else if (quote[i][0] == '\'')
 		{
@@ -85,63 +137,62 @@ static int	get_nb_args(char **quote)
 			{
 				i++;
 				last_pos = ft_strlen(quote[i]) - 1;
+				good_quote[j] = strjoin(good_quote[j], quote[i]);
 			}
-			count++;
 		}
 		else
-			count++;
-	}
-	return (count);
-}
-
-static char	**get_args_w_quote(char **quote, int nb_args, char *line_quote)
-{
-	char	**good_quote;
-	int		i;
-	int		j;
-	char	*trim;
-	char	*tmp;
-
-
-	j = 0;
-	good_quote = ft_calloc(nb_args + 1, sizeof(char *));
-	trim = ft_strtrim(line_quote, " 	");
-	tmp = trim;
-	while (*trim)
-	{
-		i = 0;
-		if (*trim == '\"')
-		{
-			i++;
-			while (trim[i] != '\"')
-				i++;
-			good_quote[j] = ft_substr(trim, 0, i + 1);
-			trim += i + 1;
-			trim = ft_strtrim(trim, " 	");
-		}
-		else if (*trim == '\'')
-		{
-			i++;
-			while (trim[i] != '\'')
-				i++;
-			good_quote[j] = ft_substr(trim, 0, i + 1);
-			trim += i + 1;
-			trim = ft_strtrim(trim, " 	");
-		}
-		else
-		{
-			while (*trim == ' ')
-				trim++;
-			while (trim[i] != ' ')
-				i++;
-			good_quote[j] = ft_substr(trim, 0, i);
-			trim += i + 1;
-		}
+			printf("pas encore gere\n");
 		j++;
 	}
-	free(tmp);
 	return (good_quote);
 }
+
+// static char	**get_args_w_quote(char **quote, int nb_args, char *line_quote)
+// {
+// 	char	**good_quote;
+// 	int		i;
+// 	int		j;
+// 	char	*trim;
+// 	char	*tmp;
+// 	j = 0;
+// 	good_quote = ft_calloc(nb_args + 1, sizeof(char *));
+// 	trim = ft_strtrim(line_quote, " 	");
+// 	tmp = trim;
+// 	while (*trim)
+// 	{
+// 		i = 0;
+// 		if (*trim == '\"')
+// 		{
+// 			i++;
+// 			while (trim[i] != '\"')
+// 				i++;
+// 			good_quote[j] = ft_substr(trim, 0, i + 1);
+// 			trim += i + 1;
+// 			trim = ft_strtrim(trim, " 	");
+// 		}
+// 		else if (*trim == '\'')
+// 		{
+// 			i++;
+// 			while (trim[i] != '\'')
+// 				i++;
+// 			good_quote[j] = ft_substr(trim, 0, i + 1);
+// 			trim += i + 1;
+// 			trim = ft_strtrim(trim, " 	");
+// 		}
+// 		else
+// 		{
+// 			while (*trim == ' ')
+// 				trim++;
+// 			while (trim[i] != ' ')
+// 				i++;
+// 			good_quote[j] = ft_substr(trim, 0, i);
+// 			trim += i + 1;
+// 		}
+// 		j++;
+// 	}
+// 	free(tmp);
+// 	return (good_quote);
+// }
 
 static char	**args_w_quote(char *line_quote)
 {
@@ -156,6 +207,8 @@ static char	**args_w_quote(char *line_quote)
 		i++;
 	if (i == 1)
 		return (quote);
+	for(int i = 0; quote[i]; i++)
+		printf("quote .%s.\n", quote[i]);
 	nb_args = get_nb_args(quote);
 	printf("klsd %d\n", nb_args);
 	good_quote = get_args_w_quote(quote, nb_args, line_quote);
@@ -164,7 +217,7 @@ static char	**args_w_quote(char *line_quote)
 
 int main()
 {
-	char **quote = args_w_quote("\"val1\",\"val2\"p");
+	char **quote = args_w_quote("\"jkdshfkjdshfk\",\"jsdhfk jdh\" \"rrrrrrr\"dfsdsf");
 	for(int i = 0; quote[i]; i++)
 		printf(".%s.\n", quote[i]);
 }
